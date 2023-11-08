27667993484a08bc8ef6c8a6eafafbbe513266c9
Jeferson <jeferson.cayo16@gmail.com>
2023-11-08 09:14:52 -0500

    avances intranet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { NewsComponent } from './news/news.component';
-import { AssistsComponent } from './assists/assists.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NewsComponent } from './pages/news/news.component';
+import { AssistsComponent } from './pages/assists/assists.component';
+import { EvaluationsComponent } from './pages/evaluations/evaluations.component';
+import { QuestionnaireComponent } from './pages/questionnaire/questionnaire.component';
+import { NewsDetailComponent } from './pages/news-detail/news-detail.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'news', component: NewsComponent },
+  { path: 'news-detail', component: NewsDetailComponent },
   { path: 'assists', component: AssistsComponent },
+  { path: 'evaluations', component: EvaluationsComponent },
+  { path: 'questionnaire', component: QuestionnaireComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NewsComponent } from './pages/news/news.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AssistsComponent } from './assists/assists.component';
+import { AssistsComponent } from './pages/assists/assists.component';
+import { EvaluationsComponent } from './pages/evaluations/evaluations.component';
+import { QuestionnaireComponent } from './pages/questionnaire/questionnaire.component';
+import { NewsDetailComponent } from './pages/news-detail/news-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NewsComponent,
-    AssistsComponent
+    AssistsComponent,
+    EvaluationsComponent,
+    QuestionnaireComponent,
+    NewsDetailComponent
   ],
   imports: [
     BrowserModule,
