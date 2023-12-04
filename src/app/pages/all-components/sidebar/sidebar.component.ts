36ea662f32d26aca0f1582b2cd9396352651e517
Bruno Andre Chavez Peralta <bchavezp@benelima.pe>
2023-12-04 17:25:59 -0500

    Modals

--- src/app/pages/all-components/sidebar/sidebar.component.ts
+++ src/app/pages/all-components/sidebar/sidebar.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  misionFlag: boolean = false;
+  visionFlag: boolean = false;
+  orgFlag: boolean = false;
+  valoresFlag: boolean = false;
 
+  fullscreen = false;
+  toggleMision(): boolean{
+    this.misionFlag=!this.misionFlag
+  return this.misionFlag
+  }
+  toggleVision(): boolean{
+    this.visionFlag=!this.visionFlag
+  return this.visionFlag
+  }
+
+  toggleOrg(): boolean{
+    this.orgFlag=!this.orgFlag
+  return this.orgFlag
+  
+  }
+  toggleValores(): boolean{
+    this.valoresFlag=!this.valoresFlag
+  return this.valoresFlag
+  }
 }
