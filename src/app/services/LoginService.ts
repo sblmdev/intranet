bbd5fdf47d1347a68892a6b67b66a1bf4578b7ba
Jeffrey Vargas <jeffrey.vargas@hacom-tech.com>
2024-01-16 16:10:18 -0500

    Update

--- src/app/services/LoginService.ts
+++ src/app/services/LoginService.ts
@@ -9,10 +9,17 @@ import { Usuario } from '../models/usuario';
 })
 export class LoginService {
   private baseUrl = 'http://192.168.1.6:8080/intranet-api/api/usuarios';
+  private baseUrlAuth = 'http://192.168.1.6:8080/intranet-api/api/auth';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<Usuario> {
+    const url = `${this.baseUrlAuth}/authenticate`;
+    const body = { username, password };
+    return this.http.post<Usuario>(url, body);
+  }
+
+  login2(username: string, password: string): Observable<Usuario> {
     const url = `${this.baseUrl}/buscarPorUsernameYContrasena/${username}/${password}`;
     return this.http.get<Usuario>(url);
   }
