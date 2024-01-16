efef4874b0e77e736a2a48631b76e84ffad84405
Bruno Chavez <bchavezp2@gmail.com>
2024-01-16 16:16:10 -0500

    Profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -12,8 +12,15 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ProfileComponent {
   @Input() dni: string = '';
   @Output() evento = new EventEmitter<any>();
-
+  user_dni:number=0;
+  user_dependency:string='';
+  user_area:string='';
+  user_location:string='';
+  user_telf:string=''
+  user_position:string='';
+  user_regime:string=''
   enviarEvento() {
     this.evento.emit('Hola desde el hijo');
   }
+  documents:string[] = [];
 }
