b721de2d760800d8bed2d949e50abc816514ab6a
sblmdev <jcayo@beneficenciadelima.org>
2024-01-24 11:04:54 -0500

    update

--- src/app/pages/recomendations/recomendations.component.ts
+++ src/app/pages/recomendations/recomendations.component.ts
@@ -97,26 +97,7 @@ export class RecomendationsComponent {
   clearData(){
     this.recomendation = new Recomendation();
     this.nuevoFlag = false;
-    this.altas = 0;
-    this.medias = 0;
-    this.bajas = 0;
-
-    this.asignadas = 0;
-    this.sinAsignar = 0;
-
-    this.fechas = 0;
-    this.sinFechas = 0;
-
-    this.contadorPorExpirar = 0;
-    this.contadorVencido = 0;
-    this.contadorOk = 0;
-
-    let fechaHoy = new Date();
-    let año = fechaHoy.getFullYear();
-    let mes = (fechaHoy.getMonth() + 1).toString().padStart(2, '0');
-    let dia = fechaHoy.getDate().toString().padStart(2, '0');
-    let fechaHoyString = `${año}-${mes}-${dia}`;
-
+    
     this.recomendationService.getRecomendationesByIdPlan(this.id).subscribe({
       next: (data) => {
         this.recomendations = JSON.parse(JSON.stringify(data));
@@ -127,115 +108,11 @@ export class RecomendationsComponent {
           for(const areaAux of areasAux){
             this.filtroAreas.push(areaAux);
           }
-
-          switch(recomendacion.nivelRiesgo) {
-            case 1: this.bajas++; break;
-            case 2: this.medias++; break;
-            case 3: this.altas++; break;
-          }
-
-          if(recomendacion.nombresResponsable == undefined || recomendacion.nombresResponsable == null || recomendacion.nombresResponsable.length == 0){
-            this.sinAsignar++;
-          }
-          else{
-            this.asignadas++
-          }
-
-          if(recomendacion.fechaFinal == undefined || recomendacion.fechaFinal == null || recomendacion.fechaFinal.length == 0){
-            this.sinFechas++
-          }
-          else{
-            this.fechas++;
-
-            let fechaFin = new Date(recomendacion.fechaFinal);
-            fechaHoy = new Date(fechaHoyString);
-
-            let diferenciaEnMilisegundos = fechaFin.getTime() - fechaHoy.getTime();
-            let diferenciaEnDias = diferenciaEnMilisegundos / (1000 * 3600 * 24);
-
-            if (diferenciaEnDias < 0) {
-              this.contadorVencido++;
-            } else if (diferenciaEnDias >= 0 && diferenciaEnDias <= 14) {
-              this.contadorPorExpirar++;
-            } else {
-              this.contadorOk++;
-            }
-          }
         }
 
         this.filtroAreas = Array.from(new Set(this.filtroAreas)).sort();
 
-        CanvasJS.addColorSet("colorRiesgos",["rgb(220 38 38)","rgb(252 211 77)","rgb(74 222 128)"]);
-        this.chartOptions = {
-          animationEnabled: true,
-          colorSet: "colorRiesgos",
-          title: {
-            text: "Observaciones por nivel de riesgo - " + this.recomendations.length + " Observaciones"
-          },
-          data: [{
-            type: "pie",
-            startAngle: -90,
-            indexLabel: "{name}: {y}",
-            yValueFormatString: "#,###.##'%'",
-            dataPoints: [
-              { y: this.altas * 100 / this.recomendations.length, name: "Alta (" + this.altas + "/" + this.recomendations.length + ")" },
-              { y: this.medias * 100 / this.recomendations.length, name: "Media (" + this.medias + "/" + this.recomendations.length + ")" },
-              { y: this.bajas * 100 / this.recomendations.length, name: "Baja (" + this.bajas + "/" + this.recomendations.length + ")" },
-            ]
-          }]
-        };
-
-        this.chartOptions2 = {
-          animationEnabled: true,
-          title: {
-          text: "Recomendaciones asignadas - " + this.recomendations.length + " Recomendaciones"
-          },
-          data: [{
-          type: "pie",
-          startAngle: -90,
-          indexLabel: "{name}: {y}",
-          yValueFormatString: "#,###.##'%'",
-          dataPoints: [
-            { y: this.asignadas*100/this.recomendations.length, name: "Asignadas (" + this.asignadas + "/" +  this.recomendations.length + ")"},
-            { y: this.sinAsignar*100/this.recomendations.length, name: "Sin asignar (" + this.sinAsignar + "/" +  this.recomendations.length + ")"},
-          ]
-          }]
-        }
-
-        this.chartOptions3 = {
-          animationEnabled: true,
-          title: {
-          text: "Recomendaciones con fechas - " + this.recomendations.length + " Recomendaciones"
-          },
-          data: [{
-          type: "pie",
-          startAngle: -90,
-          indexLabel: "{name}: {y}",
-          yValueFormatString: "#,###.##'%'",
-          dataPoints: [
-            { y: this.fechas*100/this.recomendations.length, name: "Con fecha final (" + this.fechas + "/" +  this.recomendations.length + ")"},
-            { y: this.sinFechas*100/this.recomendations.length, name: "Sin fecha final (" + this.sinFechas + "/" +  this.recomendations.length + ")"},
-          ]
-          }]
-        }
-
-        this.chartOptions4 = {
-          animationEnabled: true,
-          title: {
-          text: "Estado de fechas - " + this.fechas + " Recomendaciones"
-          },
-          data: [{
-          type: "pie",
-          startAngle: -90,
-          indexLabel: "{name}: {y}",
-          yValueFormatString: "#,###.##'%'",
-          dataPoints: [
-            { y: this.contadorOk*100/this.fechas, name: "En proceso (" + this.contadorOk + "/" +  this.fechas + ")"},
-            { y: this.contadorVencido*100/this.fechas, name: "Vencidos (" + this.contadorVencido + "/" +  this.fechas + ")"},
-            { y: this.contadorPorExpirar*100/this.fechas, name: "Por expirar (" + this.contadorPorExpirar + "/" +  this.fechas + ")"},
-          ]
-          }]
-        }
+        this.definirGraficos();
       },
       error: (_error) => {
         console.log(_error);
@@ -285,6 +162,140 @@ export class RecomendationsComponent {
           case 'Bajo': this.recomendations = this.recomendations.filter(r => r.nivelRiesgo==1); break;
         }
       }
+    }
+    this.definirGraficos();
+  }
+
+  definirGraficos() {
+    this.altas = 0;
+    this.medias = 0;
+    this.bajas = 0;
+
+    this.asignadas = 0;
+    this.sinAsignar = 0;
+
+    this.fechas = 0;
+    this.sinFechas = 0;
+
+    this.contadorPorExpirar = 0;
+    this.contadorVencido = 0;
+    this.contadorOk = 0;
+
+    let fechaHoy = new Date();
+    let año = fechaHoy.getFullYear();
+    let mes = (fechaHoy.getMonth() + 1).toString().padStart(2, '0');
+    let dia = fechaHoy.getDate().toString().padStart(2, '0');
+    let fechaHoyString = `${año}-${mes}-${dia}`;
+
+    for(const recomendacion of this.recomendations) {
+      switch(recomendacion.nivelRiesgo) {
+        case 1: this.bajas++; break;
+        case 2: this.medias++; break;
+        case 3: this.altas++; break;
+      }
+
+      if(recomendacion.nombresResponsable == undefined || recomendacion.nombresResponsable == null || recomendacion.nombresResponsable.length == 0){
+        this.sinAsignar++;
+      }
+      else{
+        this.asignadas++
+      }
+
+      if(recomendacion.fechaFinal == undefined || recomendacion.fechaFinal == null || recomendacion.fechaFinal.length == 0){
+        this.sinFechas++
+      }
+      else{
+        this.fechas++;
+
+        let fechaFin = new Date(recomendacion.fechaFinal);
+        fechaHoy = new Date(fechaHoyString);
+
+        let diferenciaEnMilisegundos = fechaFin.getTime() - fechaHoy.getTime();
+        let diferenciaEnDias = diferenciaEnMilisegundos / (1000 * 3600 * 24);
+
+        if (diferenciaEnDias < 0) {
+          this.contadorVencido++;
+        } else if (diferenciaEnDias >= 0 && diferenciaEnDias <= 14) {
+          this.contadorPorExpirar++;
+        } else {
+          this.contadorOk++;
+        }
+      }
+    }
+
+    CanvasJS.addColorSet("colorRiesgos",["rgb(220 38 38)","rgb(252 211 77)","rgb(74 222 128)"]);
+    CanvasJS.addColorSet("colorSino",["rgb(74 222 128)","rgb(220 38 38)"]);
+    this.chartOptions = {
+      animationEnabled: true,
+      colorSet: "colorRiesgos",
+      title: {
+        text: "Observaciones por nivel de riesgo - " + this.recomendations.length + " Observaciones"
+      },
+      data: [{
+        type: "pie",
+        startAngle: -90,
+        indexLabel: "{name}: {y}",
+        yValueFormatString: "#,###.##'%'",
+        dataPoints: [
+          { y: this.altas * 100 / this.recomendations.length, name: "Alta (" + this.altas + "/" + this.recomendations.length + ")" },
+          { y: this.medias * 100 / this.recomendations.length, name: "Media (" + this.medias + "/" + this.recomendations.length + ")" },
+          { y: this.bajas * 100 / this.recomendations.length, name: "Baja (" + this.bajas + "/" + this.recomendations.length + ")" },
+        ]
+      }]
+    };
+
+    this.chartOptions2 = {
+      animationEnabled: true,
+      colorSet: "colorSino",
+      title: {
+      text: "Observaciones por asignación de áreas - " + this.recomendations.length + " Observaciones"
+      },
+      data: [{
+      type: "pie",
+      startAngle: -90,
+      indexLabel: "{name}: {y}",
+      yValueFormatString: "#,###.##'%'",
+      dataPoints: [
+        { y: this.asignadas*100/this.recomendations.length, name: "Asignadas (" + this.asignadas + "/" +  this.recomendations.length + ")"},
+        { y: this.sinAsignar*100/this.recomendations.length, name: "Sin asignar (" + this.sinAsignar + "/" +  this.recomendations.length + ")"},
+      ]
+      }]
+    }
+
+    this.chartOptions3 = {
+      animationEnabled: true,
+      colorSet: "colorSino",
+      title: {
+      text: "Observaciones por asignación de fechas - " + this.recomendations.length + " Observaciones"
+      },
+      data: [{
+      type: "pie",
+      startAngle: -90,
+      indexLabel: "{name}: {y}",
+      yValueFormatString: "#,###.##'%'",
+      dataPoints: [
+        { y: this.fechas*100/this.recomendations.length, name: "Con fecha final (" + this.fechas + "/" +  this.recomendations.length + ")"},
+        { y: this.sinFechas*100/this.recomendations.length, name: "Sin fecha final (" + this.sinFechas + "/" +  this.recomendations.length + ")"},
+      ]
+      }]
+    }
+
+    this.chartOptions4 = {
+      animationEnabled: true,
+      title: {
+      text: "Estado de fechas - " + this.fechas + " Recomendaciones"
+      },
+      data: [{
+      type: "pie",
+      startAngle: -90,
+      indexLabel: "{name}: {y}",
+      yValueFormatString: "#,###.##'%'",
+      dataPoints: [
+        { y: this.contadorOk*100/this.fechas, name: "En proceso (" + this.contadorOk + "/" +  this.fechas + ")"},
+        { y: this.contadorVencido*100/this.fechas, name: "Vencidos (" + this.contadorVencido + "/" +  this.fechas + ")"},
+        { y: this.contadorPorExpirar*100/this.fechas, name: "Por expirar (" + this.contadorPorExpirar + "/" +  this.fechas + ")"},
+      ]
+      }]
     }
   }
 
